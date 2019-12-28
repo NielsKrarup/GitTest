06bbd923fad0e7f5f7d8571b6d5f8d0a35ede91e
NielsKrarup <krarup.niels@gmail.com>
2019-12-28 17:46:57 +0100

    keep it simple Master

--- funs.R
+++ funs.R
@@ -1,10 +1,8 @@
 #work on two functions independently
 foo1 <- function(a = 14) {
   # Work to do, User1 - done
-  cnd <- as.integer(a %% 3 == 0)
-  
-  out <- ifelse(cnd, "divisible by 3", "not divisible by three")
-  paste0(a, " is ", out)
+
+  return("DONE DONE")
 }
 
 foo2 <- function(x){
